2d77967cdfbf3bb2ca91149c6288372987a7d558
{sasakir23} <{r_sasaki@c-crea.jp}>
2021-10-05 10:00:12 +0900

    first

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog_app.urls')),
+    path('', include('user_app.urls')),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
